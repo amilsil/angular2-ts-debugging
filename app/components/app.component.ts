22a14e1a2050d3f44b6166c7aa141ed2c70321b5
amilsil <amilsil@gmail.com>
2016-08-23 23:00:00 +1000

    initial

---
+++ app/components/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <div class="container">
+            debugging with ts
+        </div>
+        `
+})
+
+export class AppComponent {
+    title = 'my breakpoint'
+ }
